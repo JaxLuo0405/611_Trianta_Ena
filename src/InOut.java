4a8d049f3d30127145300c7078da6fde2070b0ad
qychen <qychen@bu.edu>
2022-10-10 12:26:28 -0400

    added functions for each turn

--- src/InOut.java
+++ src/InOut.java
@@ -24,13 +24,25 @@ class InOut{
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Would you like to bet or fold? (b/f)");
 		String betOrFold = scan.next();
-		if(betOrFold=="f")
+		if(betOrFold.equals("f"))
 			return 0;
 		System.out.println("How much would you like to bet?");
 		return scan.nextInt();
 	}
 	
-	//public static void print_table(table)
+	public static void print_table(Table table){
+		System.out.println(table);
+	}
+	
+	//@return: boolean hit
+	public static boolean hit_or_stand(){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Would you like to hit or stand? (h/s)");
+		String hitOrStand = scan.next();
+		if(hitOrStand.equals("h"))
+			return true;
+		return false;
+	}
 	
 	public static void main(String[] args) {
 	}
