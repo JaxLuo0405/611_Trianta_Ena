943af65b9f2e044dc3ac03cdc5f087e0d64cc270
JaxLuo0405 <jaxluo@protonmail.com>
2022-10-10 13:42:45 -0400

    add try catch exception check for num_players and get_dealer method in InOut.java

--- src/InOut.java
+++ src/InOut.java
@@ -3,24 +3,68 @@ package src;
 import java.util.*;
 
 class InOut{
-	
+
+
 	public static int num_players(){
-		Scanner scan = new Scanner(System.in);
+
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Welcome to play Trianta Ena! Please enter the number of players (including dealer):");
-		return scan.nextInt();
+		boolean correctInput = false;
+		int result = 0;
+		while (!correctInput) {
+			String userInputLine = scanner.nextLine();
+			try {
+				int userInput = Integer.parseInt(userInputLine);
+				result = userInput;
+				if (result > 2 && result < 35 ) {
+					correctInput = true;
+				} else {
+					System.out.println("Incorrect input, please enter again");
+
+				}
+			} catch (Exception e) {
+				System.out.println("Please enter a correct choice");
+
+			}
+		}
+
+		return result;
 	}
 	
-	public static int get_dealer(){
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Which player would you like to be the dealer?");
-		return scan.nextInt();
+	public static int get_dealer(int playerNum){
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Which player index would you like to be the dealer?");
+		boolean correctInput = false;
+		int result = 0;
+		while (!correctInput) {
+			String userInputLine = scanner.nextLine();
+			try {
+				int userInput =Integer.parseInt(userInputLine);
+				result = userInput;
+				if (result < playerNum && result >= 0 ) {
+					correctInput = true;
+				} else {
+					System.out.println("Incorrect input, please enter again");
+
+				}
+			} catch (Exception e) {
+				System.out.println("Please enter a correct choice");
+
+			}
+		}
+
+		return result;
+
 	}
 	
 	public static void start_round(int roundNum){
 		System.out.println("Round "+roundNum+" starts!");
 	}
-	
+
+
 	public static int ask_player_bet(){
+
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Would you like to bet or fold? (b/f)");
 		String betOrFold = scan.next();
@@ -32,6 +76,7 @@ class InOut{
 	
 	public static void print_table(Table table){
 		System.out.println(table);
+
 	}
 	
 	//@return: boolean hit
@@ -45,5 +90,8 @@ class InOut{
 	}
 	
 	public static void main(String[] args) {
+		InOut test = new InOut();
+		test.num_players();
+		test.get_dealer(35);
 	}
 }
